7276d44f01fde28dfdbcab5e0e6b17c7423c9086
Jason Starkman <jrstarkman@gmail.com>
2023-01-31 00:32:22 -0500

    Initial Commit

---
+++ Poliquicks/PoliquicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoliquicksApp.swift
+//  Poliquicks
+//
+//  Created by Jason Starkman on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct PoliquicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PoliquicksUITests/PoliquicksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PoliquicksUITestsLaunchTests.swift
+//  PoliquicksUITests
+//
+//  Created by Jason Starkman on 1/31/23.
+//
+
+import XCTest
+
+final class PoliquicksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
